969f9ab282bf091a0021704be331f91bbc9d9d12
Axi404 <118950647+Axi404@users.noreply.github.com>
2024-06-23 22:46:29 +0800

    Update generate_markdown.py

--- scripts/generate_markdown.py
+++ scripts/generate_markdown.py
@@ -16,6 +16,7 @@ for entry in data['schools']:
 markdown_lines = []
 markdown_lines.append("# CSWinterCamp2024 & CSSummerCamp2024")
 markdown_lines.append("")
+markdown_lines.append(">[!IMPORTANT] ")
 markdown_lines.append(">1. 关于**2024年CS推免冬令营、夏令营通知公告**的汇总。欢迎大家积极分享夏令营信息，资瓷一下互联网精神吼不吼啊？（本仓库将随各校通知的发布实时更新，具体形式可参见往年：[CSSummerCamp2023](https://github.com/CS-BAOYAN/CSSummerCamp2023/tree/main)、[CSSummerCamp2022](https://github.com/LinghaoChan/CSSummerCamp2022)、[CSSummerCamp2021](https://github.com/hit-thusz-RookieCJ/CSSummerCamp2021)、[CSSummerCamp2020](https://github.com/hcy226/CSSummerCamp2020)）。")
 markdown_lines.append(">")
 markdown_lines.append(">2. 另附[CS-BAOYAN-2024](https://github.com/CS-BAOYAN/CS-BAOYAN-2024)，这是一份保研小白必看的往年经验贴大全，建议全部看完至少掌握保研流程，否则容易问一些很显然的问题")
