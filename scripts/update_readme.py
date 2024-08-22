3dc10004866a8b518213d5119e5fe4c2c69881ce
Axi404 <axihelloworld@gmail.com>
2024-08-22 23:24:17 +0800

    update update readme

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -4,7 +4,9 @@ def update_third_line(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    utc_now = datetime.datetime.now()
+    beijing_time = utc_now + datetime.timedelta(hours=8)
+    current_time = beijing_time.strftime("%Y-%m-%d %H:%M:%S")
 
     if len(lines) >= 5:
         lines[4] = f"最后修改日期为：{current_time}\n"
