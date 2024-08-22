ae7cae35ad0a8329f365b0dc219c2fcbae221a6a
Axi404 <axihelloworld@gmail.com>
2024-08-22 23:18:02 +0800

    add update readme function

---
+++ scripts/update_readme.py
@@ -0,0 +1,20 @@
+import datetime
+
+def update_third_line(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if len(lines) >= 5:
+        lines[4] = f"最后修改日期为：{current_time}\n"
+    else:
+        print("文件少于 5 行，无法替换第 5 行。")
+
+    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
+
+file_path = 'README.md'
+update_third_line(file_path)
